303cf11a7200119ae41411602bd86b7ecec37418
BjornLow <11698329+bjorn1213@users.noreply.github.com>
2024-12-01 11:56:47 +0100

    implement post

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -15,6 +15,18 @@ function getMessages(req, res) {
 	res.render("index", { messages: messages });
 }
 
+function getNewMessage(req, res) {
+	res.render("./messages/new_message");
+}
+
+function postNewMessage(req, res) {
+	const { messageText, messageUser } = req.body;
+	messages.push({ text: messageText, user: messageUser, added: new Date() });
+	res.redirect("/");
+}
+
 module.exports = {
 	getMessages,
+	getNewMessage,
+	postNewMessage,
 };
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -4,5 +4,7 @@ const messageController = require("../controllers/messageController");
 const messageRouter = Router();
 
 messageRouter.get("/", messageController.getMessages);
+messageRouter.get("/new", messageController.getNewMessage);
+messageRouter.post("/new", messageController.postNewMessage);
 
 module.exports = messageRouter;
