3a3fefae0f5c66391a5d5c50e8a051a76644780b
BjornLow <11698329+bjorn1213@users.noreply.github.com>
2024-12-01 13:22:40 +0100

    Add detail view

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,16 +3,28 @@ const messages = [
 		text: "Hi there!",
 		user: "Amando",
 		added: new Date(),
+		id: 0,
 	},
 	{
 		text: "Hello World!",
 		user: "Charles",
 		added: new Date(),
+		id: 1,
 	},
 ];
 
 function getMessages(req, res) {
-	res.render("index", { messages: messages });
+	res.render("./messages/index", { messages: messages });
+}
+
+function getMessage(req, res) {
+	const { messageId } = req.params;
+	const message = messages[messageId];
+	if (message === undefined) {
+		res.render("404");
+	} else {
+		res.render("./messages/message_detail", { message });
+	}
 }
 
 function getNewMessage(req, res) {
@@ -21,11 +33,17 @@ function getNewMessage(req, res) {
 
 function postNewMessage(req, res) {
 	const { messageText, messageUser } = req.body;
-	messages.push({ text: messageText, user: messageUser, added: new Date() });
+	messages.push({
+		text: messageText,
+		user: messageUser,
+		added: new Date(),
+		id: messages.length,
+	});
 	res.redirect("/");
 }
 
 module.exports = {
+	getMessage,
 	getMessages,
 	getNewMessage,
 	postNewMessage,
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -6,5 +6,6 @@ const messageRouter = Router();
 messageRouter.get("/", messageController.getMessages);
 messageRouter.get("/new", messageController.getNewMessage);
 messageRouter.post("/new", messageController.postNewMessage);
+messageRouter.get("/:messageId", messageController.getMessage);
 
 module.exports = messageRouter;
